c25798f7b9df4b1217d166b405047c44f3227aba
Caleb O'Malley <calebomalley14@gmail.com>
2024-11-05 14:49:26 -0500

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // both args must be integers
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR'
+    }
+    // doesn't work with negative numbers
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    }
+    // makes sure that start is less than end
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    
+    // main logic
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
