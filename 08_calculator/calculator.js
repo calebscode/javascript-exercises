96051a023f612f6d66f217b6ad3d26a8fb417d0b
Caleb O'Malley <calebomalley14@gmail.com>
2024-11-10 21:52:01 -0500

    completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, current) => {
+    total += current;
+    return total;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, current) => {
+    product *= current;
+    return product;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a < 0) return -1;
+  if (a < 2) return 1;
+  let output = 1;
+  for (let i = a; i > 1; i--) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
