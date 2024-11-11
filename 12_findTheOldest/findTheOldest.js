23dcfcd862f3c9770d19046ece2aeb1b9627c85f
Caleb O'Malley <calebomalley14@gmail.com>
2024-11-10 22:48:28 -0500

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const now = new Date();
+    const thisYear = now.getFullYear();
+    let sortedByAge = people.sort((a, b) => {
+        age1 = (a.yearOfDeath || thisYear) - a.yearOfBirth;
+        age2 = (b.yearOfDeath || thisYear) - b.yearOfBirth;
+        return age2 - age1;
+    })
+    
+    return sortedByAge[0];
 };
 
 // Do not edit below this line
