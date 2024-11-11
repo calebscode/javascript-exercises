639270e5dcdc0499f6c41335c6918d01f60b776e
Caleb O'Malley <calebomalley14@gmail.com>
2024-11-10 22:21:53 -0500

    solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    if (str.length < 3) return true;
+    // sanitize the string
+    let chars = Array.from(str.toLowerCase());
+    chars = chars.filter((char) => isAlphanumeric(char));
 
+    let a = 0;
+    let b = chars.length-1;
+    while (b - a > 0) {
+        if (chars[a] != chars[b]) return false;
+        a++;
+        b--;
+    }
+    return true;
 };
+
+function isAlphanumeric(char) {
+    return char.match(/^\w+$/) != null;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
