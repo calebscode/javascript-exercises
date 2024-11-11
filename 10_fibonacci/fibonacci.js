c5b5e67002c2b7344656da34a784b210d2a0703a
Caleb O'Malley <calebomalley14@gmail.com>
2024-11-10 22:34:14 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+// 1, 1, 2, 3, 5, 8, 13, ...
 
+const fibonacci = function(n) {
+    // base cases
+    if (!Number.isInteger(n)) n = parseInt(n);
+    if (n < 0) return 'OOPS';
+    if (n == 0) return 0;
+    if (n < 3) return 1;
+    //recursive call
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
